69eff8083f1af1983bc1c0cbc27e619c56bc01d1
lizac9 <lizacarelli@gmail.com>
2022-08-30 09:07:31 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubstractQuestions(){
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
